705077632cb4d0e5fc0fd59170b21694925cd27e
Boccia <fboccia96@gmail.com>
2022-03-22 10:17:49 +0000

    Still wrapping my head around this numbers

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 "use script";
 
 const cardNumber = document.getElementById("cardNumber");
-const cardNumberUser = document.querySelector(".frontCard__cardNumber");
+const cardNumberInput = document.querySelector(".frontCard__cardNumber");
+const cardNumbersDiv = document.querySelectorAll(
+  ".frontCard__cardNumber__numbers"
+);
 const cardName = document.getElementById("cardName");
 const cardNameUser = document.querySelector(".frontCard__holder__name");
 const optionMonth = document.getElementById("optionMonth");
 const optionYear = document.getElementById("optionYear");
 const expiresDate = document.querySelector(".frontCard__expires__date");
 
-// Function Check if number are correct
+const cardValue = document.querySelector(".frontCard__cardNumber__numbers");
 
-// function checkLenght(array) {
-//   if (array.length <= 4) {
-//     console.log(array);
-//   } else {
-//     array.pop();
-//     alert("No More Then 4!");
-//   }
-// }
+cardNumber.addEventListener("input", function () {
+  const value = cardNumber.value;
+  const a = value.split(" ");
+  cardNumberInput.innerHTML = `
+  <div class="frontCard__cardNumber__numbers">${a[0]}</div>
+  <div class="frontCard__cardNumber__numbers">${a[1]}</div>
+  <div class="frontCard__cardNumber__numbers">${a[2]}</div>
+  <div class="frontCard__cardNumber__numbers">${a[3]}</div>`;
+  console.log(cardNumbersDiv);
+  cardNumbersDiv.any((div) => div === undefined);
+});
 
-// cardNumber.addEventListener("input", function () {
-//   let array = [];
-
-//   const value = cardNumber.value;
-//   const arrayNumber = array.push(value);
-//   console.log(arrayNumber);
-//   // arrayNumber.forEach(function (a, i) {
-//   //   console.log(a, i);
-//   // });
+// const a = value.split(" ");
+// cardNumberUser.forEach((a, index, array) => {
+//   console.log(a, index, array);
 //   cardNumberUser.innerHTML = `
-//     <div class="frontCard__cardNumber__numbers">${arrayNumber[0]}</div>
-//     <div class="frontCard__cardNumber__numbers">${arrayNumber[1]}</div>
-//     <div class="frontCard__cardNumber__numbers">${arrayNumber[2]}</div>
-//     <div class="frontCard__cardNumber__numbers">${arrayNumber[3]}</div>`;
+//     <div class="frontCard__cardNumber__numbers">${index}</div>
+//     <div class="frontCard__cardNumber__numbers">${index}</div>
+//     <div class="frontCard__cardNumber__numbers">${index}</div>
+//     <div class="frontCard__cardNumber__numbers">${index}</div>`;
 // });
 
-cardName.addEventListener("input", function () {
-  const value = cardName.value;
-  cardNameUser.innerHTML = `<div class="frontCard__cardNumber__numbers">${value}</div>`;
-});
+// cardName.addEventListener("input", function () {
+//   const value = cardName.value;
+//   cardNameUser.innerHTML = `<div class="frontCard__cardNumber__numbers">${value}</div>`;
+// });
 
-optionMonth.addEventListener("keydown", function (e) {
-  if (e.key == "Tab") {
-    const month = optionMonth.value;
-    expiresDate.innerHTML = `<div class="frontCard__expires__date">${month}/</div>`;
-  }
-});
+// optionMonth.addEventListener("keydown", function (e) {
+//   if (e.key == "Tab") {
+//     const month = optionMonth.value;
+//     expiresDate.innerHTML = `<div class="frontCard__expires__date">${month}/</div>`;
+//   }
+// });
 
-const frontCard = document.querySelector(".frontCard");
-const backCard = document.querySelector(".backCard");
-const inputCvv = document.querySelector(".options__cvvInput");
+// const frontCard = document.querySelector(".frontCard");
+// const backCard = document.querySelector(".backCard");
+// const inputCvv = document.querySelector(".options__cvvInput");
 
-inputCvv.addEventListener("focus", function () {
-  frontCard.style.transform = "rotate(360deg)";
-  frontCard.style.display = "none";
-  backCard.style.display = "block";
-});
+// inputCvv.addEventListener("focus", function () {
+//   frontCard.style.transform = "rotate(360deg)";
+//   frontCard.style.display = "none";
+//   backCard.style.display = "block";
+// });
